97adb69fda0ac19cf7e161dea6259d95e9a05a81
Noud de Brouwer <noud4@home.nl>
2020-02-23 11:53:28 +0100

    ImageController and Service

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -5,29 +5,27 @@ namespace App\Http\Controllers;
 use App\DataTypes\ImagesListDataType;
 use App\Events\MyEvent;
 use App\Models\Image;
+use App\Services\ImageService;
 
 class ImageController extends Controller
 {
+    private $shortcut;
+    
+    public function __construct(ImageService $imageService)
+    {
+        $this->imageService = $imageService;
+    }
+    
     public function popup()
     {
-        $images = Image::all();
-
-        // @todo relations
-        $images = $images->slice(0, -3);
-        // foreach ($images as $image) {
-        //     $imageCategoryType = $image->categoryType;
-        //     if ($imageCategoryType) {
-        //         dd($imageCategoryType->imageCategoryType);
-        //         dd('g');
-        //     }
-        // }
-        // dd($images);
+        $images = $this->imageService->getImageByCategoryTypeShortcut('emoticon');
 
         if ($images) {
             return view('images.popup', [
                 'data' => (new ImagesListDataType('emoticon', $images))
             ]);
         }
+        
         return 'not found';
     }
 }
--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -9,5 +9,23 @@ class ImageService
 	public static function getImageByShortcut(string $shortcut)
 	{
         return Image::where('shortcut', $shortcut)->first();
-	}
+	}        
+
+	public function getImageByCategoryTypeShortcut(string $shortcut)
+	{
+        $images = Image::all();
+
+		// @todo relations
+		$images = $images->slice(0, -3);
+		// foreach ($images as $image) {
+		//     $imageCategoryType = $image->categoryType;
+		//     if ($imageCategoryType) {
+		//         dd($imageCategoryType->imageCategoryType);
+		//         dd('g');
+		//     }
+		// }
+		// dd($images);
+
+		return $images;
+	}        
 }
