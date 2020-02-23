8e2f49a25785233fba34671ed576de6ffa940d4e
Noud de Brouwer <noud4@home.nl>
2020-02-23 22:40:01 +0100

    data

--- database/data/laravel-images.sql
+++ database/data/laravel-images.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysql
--- Generation Time: Feb 23, 2020 at 08:09 PM
+-- Generation Time: Feb 23, 2020 at 09:27 PM
 -- Server version: 5.7.26
 -- PHP Version: 7.2.19
 
@@ -33,44 +33,52 @@ INSERT INTO `accounts` (`id`, `name`, `email`, `email_verified_at`, `password`, 
 (4, 'noud41', 'noud4@home.nl', NULL, '', NULL, NULL, NULL);
 
 --
+-- Dumping data for table `extensions`
+--
+
+INSERT INTO `extensions` (`id`, `image_type_id`, `extension`, `created_at`, `updated_at`) VALUES
+(1, 4, 'jpg', NULL, NULL),
+(2, 1, 'gig', NULL, NULL);
+
+--
 -- Dumping data for table `image_categories`
 --
 
 INSERT INTO `image_categories` (`id`, `image_category_type_id`, `image_category_type_order`, `image_id`, `created_at`, `updated_at`) VALUES
-(1, 1, 1, 1, NULL, NULL),
-(2, 1, 2, 2, NULL, NULL),
-(3, 1, 3, 3, NULL, NULL),
-(4, 1, 4, 4, NULL, NULL),
-(7, 1, 5, 5, NULL, NULL),
-(8, 1, 6, 6, NULL, NULL),
-(9, 1, 7, 7, NULL, NULL),
-(10, 1, 8, 8, NULL, NULL),
-(11, 1, 9, 9, NULL, NULL),
-(12, 1, 10, 10, NULL, NULL),
-(13, 1, 11, 11, NULL, NULL),
-(14, 1, 12, 12, NULL, NULL),
-(15, 1, 13, 13, NULL, NULL),
-(16, 1, 14, 14, NULL, NULL),
-(17, 1, 15, 15, NULL, NULL),
-(18, 1, 16, 16, NULL, NULL),
-(19, 1, 17, 17, NULL, NULL),
-(20, 1, 18, 18, NULL, NULL),
-(21, 1, 19, 19, NULL, NULL),
-(22, 1, 21, 21, NULL, NULL),
-(23, 1, 22, 22, NULL, NULL),
-(24, 1, 23, 23, NULL, NULL),
-(25, 1, 24, 24, NULL, NULL),
-(26, 1, 25, 25, NULL, NULL),
-(27, 1, 26, 26, NULL, NULL),
-(28, 1, 27, 27, NULL, NULL),
-(29, 1, 28, 28, NULL, NULL),
-(30, 1, 29, 29, NULL, NULL),
-(31, 1, 30, 30, NULL, NULL),
-(32, 1, 31, 31, NULL, NULL),
-(33, 1, 32, 32, NULL, NULL),
-(34, 1, 33, 33, NULL, NULL),
-(35, 1, 34, 34, NULL, NULL),
-(36, 1, 20, 20, NULL, NULL),
+(1, 101, 1, 1, NULL, NULL),
+(2, 101, 2, 2, NULL, NULL),
+(3, 101, 3, 3, NULL, NULL),
+(4, 101, 4, 4, NULL, NULL),
+(7, 101, 5, 5, NULL, NULL),
+(8, 101, 6, 6, NULL, NULL),
+(9, 101, 7, 7, NULL, NULL),
+(10, 101, 8, 8, NULL, NULL),
+(11, 101, 9, 9, NULL, NULL),
+(12, 101, 10, 10, NULL, NULL),
+(13, 101, 11, 11, NULL, NULL),
+(14, 101, 12, 12, NULL, NULL),
+(15, 101, 13, 13, NULL, NULL),
+(16, 101, 14, 14, NULL, NULL),
+(17, 101, 15, 15, NULL, NULL),
+(18, 101, 16, 16, NULL, NULL),
+(19, 101, 17, 17, NULL, NULL),
+(20, 101, 18, 18, NULL, NULL),
+(21, 101, 19, 19, NULL, NULL),
+(22, 101, 21, 21, NULL, NULL),
+(23, 101, 22, 22, NULL, NULL),
+(24, 101, 23, 23, NULL, NULL),
+(25, 101, 24, 24, NULL, NULL),
+(26, 101, 25, 25, NULL, NULL),
+(27, 101, 26, 26, NULL, NULL),
+(28, 101, 27, 27, NULL, NULL),
+(29, 101, 28, 28, NULL, NULL),
+(30, 101, 29, 29, NULL, NULL),
+(31, 101, 30, 30, NULL, NULL),
+(32, 101, 31, 31, NULL, NULL),
+(33, 101, 32, 32, NULL, NULL),
+(34, 101, 33, 33, NULL, NULL),
+(35, 101, 34, 34, NULL, NULL),
+(36, 101, 20, 20, NULL, NULL),
 (45, 5, 1, 35, NULL, NULL),
 (46, 5, 1, 41, NULL, NULL);
 
@@ -79,7 +87,9 @@ INSERT INTO `image_categories` (`id`, `image_category_type_id`, `image_category_
 --
 
 INSERT INTO `image_types` (`id`, `name`, `created_at`, `updated_at`) VALUES
-(1, 'webp', NULL, NULL);
+(1, 'gif', NULL, NULL),
+(2, 'webp', NULL, NULL),
+(4, 'jpg', NULL, NULL);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
